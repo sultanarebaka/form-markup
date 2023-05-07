7255b5b6f7391a51e78082e45ced06412177c181
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-05-07 15:17:27 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-(document).ready(function () {
+$(document).ready(function () {
   
-    ("#SignUp_form").submit(function (e) {
+    $("#SignUp_form").submit(function (e) {
       e.preventDefault();    
   
-      var name = ("#name").val();
-      var email = ("#email").val();
-      var password = ("#password").val();
+      var name = $("#name").val();
+      var email = $("#email").val();
+      var password = $("#password").val();
       
-      (".error").remove();
+      $(".error").remove();
       
       // Check the name length, return an error if field is empty.
       if (name.length < 1) {
-        ("#name").after(
+        $("#name").after(
           '<span class="error">Name is required</span>'
         );
         return;
@@ -20,20 +20,20 @@
       // Check the email length, return an error if the field is empty.
       // Also run a regex against the email to verify the value is an email.
       if (email.length < 1) {
-        ("#email").after('<span class="error">Email is required</span>');
+        $("#email").after('<span class="error">Email is required</span>');
         return;
       } else {
         var regEx = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         var validEmail = regEx.test(email);
         if (!validEmail) {
-          ("#email").after('<span class="error">Enter a valid email</span>');
+          $("#email").after('<span class="error">Enter a valid email</span>');
           return;
         }
       }
       
       // Check the length of the password value, if empty return an error. 
       if (password.length < 8) {
-        ("#password").after(
+        $("#password").after(
           '<span class="error">Password must be at least 8 characters long</span>'
         );
         return;
@@ -54,30 +54,30 @@
   });
 
 
-('#SignUp_form').submit(function(e) {
+$('#SignUp_form').submit(function(e) {
     e.preventDefault();
  });
 
- var fieldName  = ('fieldCSSSelector').val();
- var name = ("#name").val();
-var email = ("#email").val();
-var password = ("#password").val();
+ var fieldName  = $('fieldCSSSelector').val();
+ var name = $("#name").val();
+var email = $("#email").val();
+var password = $("#password").val();
 
 // Check the name length, return an error if field is empty.
 if (name.length < 1) {
-    ("#name").after(
+    $("#name").after(
       '<span class="error">Name is required</span>');
     return;
 }
 // Check the email length, return an error if the field is empty.
 if (email.length < 1) {
-    ("#email").after('<span class="error">Email is required</span>');
+    $("#email").after('<span class="error">Email is required</span>');
     return;
  }
 
  // Check the length of the password value, if empty return an error.
 if (password.length < 8) {
-    ("#password").after('<span class="error">Password must be at least 8 characters long</span>'
+    $("#password").after('<span class="error">Password must be at least 8 characters long</span>'
   );
     return;
   }
@@ -87,12 +87,12 @@ if (password.length < 8) {
   var validEmail = regEx.test(email);
   // Also run a regex against the email to verify the value is an email.
 if (!validEmail) {
-    ("#email").after('<span class="error">Enter a valid email</span>');
+    $("#email").after('<span class="error">Enter a valid email</span>');
     return;
  }
 
 
- (".error").remove();
+ $(".error").remove();
 
   // If form has been filled send the data to a server.
  // In this case we create an alert message
